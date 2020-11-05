2c5ec6545a12536476ce56335b1ab8a74c287b08
Miroslav Šedivý <sedivy.miro@gmail.com>
2020-11-05 12:52:22 +0100

    sample interactivity.

--- src/Neko.vue
+++ src/Neko.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Hello world, Neko.</h1>
+    <button @click="visible = !visible">Toggle</button>
+    <div v-if="visible">Is this visible?</div>
   </div>
 </template>
 
@@ -10,5 +12,7 @@
   @Component({
     name: 'neko',
   })
-  export default class extends Vue {}
+  export default class extends Vue {
+    public visible: boolean = false
+  }
 </script>
