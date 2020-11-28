5699ad0bf975388f688e67a4d97c8c4a2c8dc34b
Miroslav Šedivý <sedivy.miro@gmail.com>
2020-11-28 15:22:04 +0100

    allow login using Query.

--- internal/session/auth.go
+++ internal/session/auth.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (manager *SessionManagerCtx) Authenticate(r *http.Request) (types.Session, error) {
-	id, secret, ok := r.BasicAuth()
+	id, secret, ok := getAuthData(r)
 	if !ok {
 		return nil, fmt.Errorf("no authentication provided")
 	}
@@ -23,4 +23,20 @@ func (manager *SessionManagerCtx) Authenticate(r *http.Request) (types.Session, 
 	}
 
 	return session, nil
+}
+
+func getAuthData(r *http.Request) (string, string, bool) {
+	id, secret, ok := r.BasicAuth()
+	if ok {
+		return id, secret, true
+	}
+
+	id = r.URL.Query().Get("id")
+	secret = r.URL.Query().Get("secret")
+
+	if id != "" && secret != "" {
+		return id, secret, true
+	}
+
+	return "", "", false
 }
