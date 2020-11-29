34ac9098f04927bbd2e44fd78796dd610f38c692
Miroslav Šedivý <sedivy.miro@gmail.com>
2020-11-29 16:14:09 +0100

    use proxy to backend.

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,14 @@ module.exports = {
   },
   devServer: {
     disableHostCheck: true,
+    proxy: {
+      '^/ws': {
+        target: 'ws://192.168.1.20:3000/',
+        ws: true,
+      },
+      '^/api': {
+        target: 'http://192.168.1.20:3000/',
+      },
+    },
   },
 }
