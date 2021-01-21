1def45f2a1a9128a4c0497bc852cb98067bbed91
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-01-21 16:01:23 +0100

    add vuejs wrapper for component.

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Neko from './component/main.vue'
+
+/**
+ * Fügt eine "install" function hinzu
+ *
+ * Weitere Infos:
+ *      https://vuejs.org/v2/cookbook/packaging-sfc-for-npm.html#Packaging-Components-for-npm
+ */
+const NekoElements = {
+  install(vue: typeof Vue): void {
+    vue.component('Neko', Neko);
+  },
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+  // @ts-ignore
+  window.Vue.use(NekoElements, {});
+}
+
+export { Neko };
+export default NekoElements;
