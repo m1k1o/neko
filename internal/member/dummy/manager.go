f57332ba2a65c521e9a66006a4be8fd4ea5a4fef
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-03-14 13:06:33 +0100

    dummy allow all logins.

--- internal/member/dummy/manager.go
+++ internal/member/dummy/manager.go
@@ -1,6 +1,8 @@
 package dummy
 
 import (
+	"fmt"
+
 	"demodesk/neko/internal/types"
 )
 
@@ -19,29 +21,37 @@ func (manager *MemberManagerCtx) Disconnect() error {
 }
 
 func (manager *MemberManagerCtx) Authenticate(username string, password string) (string, types.MemberProfile, error) {
-	return username, types.MemberProfile{}, nil
+	return username, types.MemberProfile{
+		Name:               username,
+		IsAdmin:            true,
+		CanLogin:           true,
+		CanConnect:         true,
+		CanWatch:           true,
+		CanHost:            true,
+		CanAccessClipboard: true,
+	}, nil
 }
 
 func (manager *MemberManagerCtx) Insert(username string, password string, profile types.MemberProfile) (string, error) {
-	return "", nil
+	return "", fmt.Errorf("Not implemented.")
 }
 
 func (manager *MemberManagerCtx) Select(id string) (types.MemberProfile, error) {
-	return types.MemberProfile{}, nil
+	return types.MemberProfile{}, fmt.Errorf("Not implemented.")
 }
 
 func (manager *MemberManagerCtx) SelectAll(limit int, offset int) (map[string]types.MemberProfile, error) {
-	return map[string]types.MemberProfile{}, nil
+	return map[string]types.MemberProfile{}, fmt.Errorf("Not implemented.")
 }
 
 func (manager *MemberManagerCtx) Update(id string, profile types.MemberProfile) error {
-	return nil
+	return fmt.Errorf("Not implemented.")
 }
 
 func (manager *MemberManagerCtx) UpdatePassword(id string, passwrod string) error {
-	return nil
+	return fmt.Errorf("Not implemented.")
 }
 
 func (manager *MemberManagerCtx) Delete(id string) error {
-	return nil
+	return fmt.Errorf("Not implemented.")
 }
