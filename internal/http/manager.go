52e586790efd2d4be6cb7546a2f9e3a17a8a4b1f
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-03-21 21:42:53 +0100

    fix static files serving Path.

--- internal/http/manager.go
+++ internal/http/manager.go
@@ -52,10 +52,10 @@ func New(WebSocketManager types.WebSocketManager, ApiManager types.ApiManager, c
 	if conf.Static != "" {
 		fs := http.FileServer(http.Dir(conf.Static))
 		router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
-			if _, err := os.Stat(conf.Static + r.RequestURI); os.IsNotExist(err) {
-				http.StripPrefix(r.RequestURI, fs).ServeHTTP(w, r)
-			} else {
+			if _, err := os.Stat(conf.Static + r.URL.Path); !os.IsNotExist(err) {
 				fs.ServeHTTP(w, r)
+			} else {
+				utils.HttpNotFound(w)
 			}
 		})
 	}
