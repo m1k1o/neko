73ea442d188795020b855925519e61ff8447d5a0
Walllessloki39 <48391622+Walllessloki39@users.noreply.github.com>
2021-04-03 22:56:03 +0200

    Update index.ts

--- client/src/locale/index.ts
+++ client/src/locale/index.ts
@@ -1,9 +1,11 @@
 import * as en from './en-us'
 import * as es from './es-sp'
 import * as sk from './sk-sk'
+import * as sv from './sv-se'
 
 export const messages = {
   en,
   es,
   sk,
+  sv
 }
