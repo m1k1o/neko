54bcbd6333be048cbadf4534c3ee1af39255ffe6
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-11-03 22:01:49 +0100

    lint cursors types.

--- src/component/types/cursors.ts
+++ src/component/types/cursors.ts
@@ -1,8 +1,20 @@
 import { CursorImage } from './webrtc'
-  
-export type CursorDrawFunction = (ctx: CanvasRenderingContext2D, x: number, y: number, cursorElement: HTMLImageElement, cursorImage: CursorImage, cursorTag: string) => void
 
-export type InactiveCursorDrawFunction = (ctx: CanvasRenderingContext2D, x: number, y: number, cursorTag: string) => void
+export type CursorDrawFunction = (
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  cursorElement: HTMLImageElement,
+  cursorImage: CursorImage,
+  cursorTag: string,
+) => void
+
+export type InactiveCursorDrawFunction = (
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  cursorTag: string,
+) => void
 
 export interface Dimension {
   width: number
