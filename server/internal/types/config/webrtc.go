f4682d3f1c856667fc3edb009d1811c07302b0d5
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-12-11 15:12:27 +0100

    use IMPLICIT_CONTROL.

--- server/internal/types/config/webrtc.go
+++ server/internal/types/config/webrtc.go
@@ -68,8 +68,8 @@ func (WebRTC) Init(cmd *cobra.Command) error {
 	}
 
 	// TODO: Should be moved to session config.
-	cmd.PersistentFlags().Bool("implicitcontrol", false, "if enabled members can gain control implicitly")
-	if err := viper.BindPFlag("implicitcontrol", cmd.PersistentFlags().Lookup("implicitcontrol")); err != nil {
+	cmd.PersistentFlags().Bool("implicit-control", false, "if enabled members can gain control implicitly")
+	if err := viper.BindPFlag("implicit-control", cmd.PersistentFlags().Lookup("implicit-control")); err != nil {
 		return err
 	}
 
@@ -130,5 +130,5 @@ func (s *WebRTC) Set() {
 	}
 
 	// TODO: Should be moved to session config.
-	s.ImplicitControl = viper.GetBool("implicitcontrol")
+	s.ImplicitControl = viper.GetBool("implicit-control")
 }
