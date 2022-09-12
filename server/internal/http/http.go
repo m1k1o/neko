4b6d9d885c3a43fc288309cef725b844b970ed7c
Miroslav Šedivý <sedivy.miro@gmail.com>
2022-09-12 22:18:18 +0200

    move config from types.

--- server/internal/http/http.go
+++ server/internal/http/http.go
@@ -11,8 +11,8 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
+	"m1k1o/neko/internal/config"
 	"m1k1o/neko/internal/types"
-	"m1k1o/neko/internal/types/config"
 )
 
 type Server struct {
