b793e5b3774b4fccaa7aadbb45933b173a35cd03
Miroslav Šedivý <sedivy.miro@gmail.com>
2023-01-12 20:34:09 +0100

    add stereo to opus codec.

--- pkg/types/codec/codecs.go
+++ pkg/types/codec/codecs.go
@@ -131,7 +131,7 @@ func Opus() RTPCodec {
 			MimeType:     webrtc.MimeTypeOpus,
 			ClockRate:    48000,
 			Channels:     2,
-			SDPFmtpLine:  "useinbandfec=1",
+			SDPFmtpLine:  "useinbandfec=1;stereo=1",
 			RTCPFeedback: []webrtc.RTCPFeedback{},
 		},
 		// https://gstreamer.freedesktop.org/documentation/opus/opusenc.html
