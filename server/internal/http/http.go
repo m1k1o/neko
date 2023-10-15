3c4d7b9d6070ee4774ec9be2fcd38a65e1bc31e8
Miroslav Šedivý <sedivy.miro@gmail.com>
2023-10-15 13:18:43 +0200

    upgrade go-chi to v5.

--- server/internal/http/http.go
+++ server/internal/http/http.go
@@ -11,8 +11,8 @@ import (
 	"regexp"
 	"strconv"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
